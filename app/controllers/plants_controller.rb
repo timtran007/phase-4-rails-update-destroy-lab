604bba2963e0d06422aaf7e78a2bcc97ccff90e4
Tim <timtran007@gmail.com>
2023-01-10 12:28:00 -0600

    commit tree 79ce1e625acf278c4c0e6abf79b8a0a0f424c3c4

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,48 @@
 class PlantsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :plant_not_found_response
 
   # GET /plants
   def index
     plants = Plant.all
     render json: plants
   end
-
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
-
+  
   # POST /plants
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+  
+  # GET /plants/:id
+  def show
+    plant = find_plant
+    render json: plant
+  end
+
+  # POST /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
   end
 
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+  end
+
+  def plant_not_found_response
+    render json: {error: "Plant not found"}, status: :not_found
   end
 end
